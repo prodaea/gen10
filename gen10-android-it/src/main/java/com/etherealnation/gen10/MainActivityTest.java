39a2f9cbd2e6f6218c4c8f0abdb12cc44b32b9dc
R. Lee Creasy <richard_creasy@epam.com>
2013-09-17 10:55:48 -0400

    initial commit with maven template and intellij project files

---
+++ gen10-android-it/src/main/java/com/etherealnation/gen10/MainActivityTest.java
@@ -0,0 +1,11 @@
+package com.etherealnation.gen10;
+
+import android.test.AndroidTestCase;
+
+/**
+ * Created by lcreasy on 9/17/13.
+ * MainActivityTest
+ */
+public class MainActivityTest extends AndroidTestCase {
+
+}
---
+++ gen10-android/src/main/java/com/etherealnation/gen10/MainActivity.java
@@ -0,0 +1,16 @@
+package com.etherealnation.gen10;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by lcreasy on 9/17/13.
+ * MainActivity
+ */
+public class MainActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
